459f7ab2d56186f456e9986b59531f2a6b5b3cb2
Fabien Bochu <fabien.bochu@polyconseil.fr>
2015-04-28 16:20:21 +0200

    Rename builder.py into grocker.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from blease.doc.sphinxbaseconf import *
 
-from builder import __version__
+from grocker import __version__
 
 
 project = u"Grocker"
