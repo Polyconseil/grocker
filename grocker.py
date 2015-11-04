8a7e7d4b81419dedff1431076849232b3d32165e
Xavier Fernandez <xavier.fernandez@polyconseil.fr>
2015-11-04 17:55:01 +0100

    Release grocker 0.8.6

--- grocker.py
+++ grocker.py
@@ -25,7 +25,7 @@ import sys
 import textwrap
 
 
-__version__ = '0.8.5'
+__version__ = '0.8.6'
 __copyright__ = '2015, Polyconseil'
 REGISTRY_FQDN = 'docker.polydev.blue'
 REQUIRED_IMAGE_NAMES = ('base', 'compiler')
