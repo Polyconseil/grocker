3e76e6e3efc68312bcdc32c1c9f28c8805bb1884
Fabien Bochu <fabien.bochu@polyconseil.fr>
2015-11-20 15:47:13 +0100

    Release Grocker 0.8.7

--- grocker.py
+++ grocker.py
@@ -25,7 +25,7 @@ import sys
 import textwrap
 
 
-__version__ = '0.8.6'
+__version__ = '0.8.7'
 __copyright__ = '2015, Polyconseil'
 REGISTRY_FQDN = 'docker.polydev.blue'
 REQUIRED_IMAGE_NAMES = ('base', 'compiler')
