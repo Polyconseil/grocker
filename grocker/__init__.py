fd5e74a0a7ce15b4418598d97525fda390c42f67
Fabien Bochu <fabien.bochu@polyconseil.fr>
2016-01-26 17:43:15 +0100

    Release Grocker 2.0a10

--- grocker/__init__.py
+++ grocker/__init__.py
@@ -3,7 +3,7 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = '2.0a9'
+__version__ = '2.0a10'
 __copyright__ = '2015, Polyconseil'
 __description__ = 'Docker image builder'
 
