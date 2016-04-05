c09b24be912ba6784c022628669291d6577432d2
François Freitag <francois.freitag@polyconseil.fr>
2016-04-05 09:22:56 +0200

    fixup: Import os.path instead of os (helpers.py)

--- grocker/helpers.py
+++ grocker/helpers.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import contextlib
 import hashlib
 import io
-import os
+import os.path
 import shutil
 import tempfile
 
