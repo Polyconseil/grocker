694913d0fa84cef7e5706f1e4e7e8a5c90372fe6
Fabien Bochu <fabien.bochu@polyconseil.fr>
2016-05-20 12:02:11 +0200

    Fix tests for Python 2

--- tests/test_build.py
+++ tests/test_build.py
@@ -39,6 +39,25 @@ def docker_run(image, command):
     return return_code, logs
 
 
+# Python 3 backport: textwrap.indent
+def indent(text, prefix, predicate=None):
+    """Adds 'prefix' to the beginning of selected lines in 'text'.
+
+    If 'predicate' is provided, 'prefix' will only be added to the lines
+    where 'predicate(line)' is True. If 'predicate' is not provided,
+    it will default to adding 'prefix' to all non-empty lines that do not
+    consist solely of whitespace characters.
+    """
+    if predicate is None:
+        def predicate(line):
+            return line.strip()
+
+    def prefixed_lines():
+        for line in text.splitlines(True):
+            yield (prefix + line if predicate(line) else line)
+    return ''.join(prefixed_lines())
+
+
 class BuildTestCase(unittest.TestCase):
     release = 'grocker-test-project==1.0.3'
     dependencies = """
@@ -90,7 +109,7 @@ class BuildTestCase(unittest.TestCase):
     def test_dependencies(self):
         config = """
             dependencies: %s
-        """ % textwrap.indent(self.dependencies, '    ')
+        """ % indent(self.dependencies, '    ')
         msg = 'Grocker build this successfully !'
         expected = msg
         self.check(config, msg, expected)
@@ -99,7 +118,7 @@ class BuildTestCase(unittest.TestCase):
         config = """
             entrypoint_name: my-custom-runner
             dependencies: %s
-        """ % textwrap.indent(self.dependencies, '    ')
+        """ % indent(self.dependencies, '    ')
         msg = 'Grocker build this successfully !'
         expected = 'custom: %s' % msg
         self.check(config, msg, expected)
