d56f432223ba9ce585155ecd0d71481ce626761a
Fabien Bochu <fabien.bochu@polyconseil.fr>
2016-11-08 15:52:23 +0100

    Add license in setup.py

--- setup.py
+++ setup.py
@@ -35,10 +35,12 @@ setup(
             'enum34',
         ],
     },
+    license='BSD',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
