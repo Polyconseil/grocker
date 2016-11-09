e5ec047faa90528c2c8b39ba1f1abca4ea701d54
Fabien Bochu <fabien.bochu@polyconseil.fr>
2016-11-09 17:50:25 +0100

    Back to development: 4.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(filename):
 
 setup(
     name='grocker',
-    version='4.3.2',
+    version='4.4.dev0',
     description="Docker image builder",
     long_description=read('Readme.rst'),
     keywords='docker build packaging',
