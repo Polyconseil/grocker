28e1a255e91a6c18fb641a151ff113f2a19ae4ad
Fabien Bochu <fabien.bochu@polyconseil.fr>
2017-02-22 12:03:14 +0100

    compiler: Use buildargs for dependencies

--- src/grocker/resources/docker/compiler-image/provision.sh
+++ src/grocker/resources/docker/compiler-image/provision.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 set -xe
 
-cd $(dirname $0)  # Go to Grocker build dir
-if [ -e ./provision.env ]; then
-    . ./provision.env  # Retrieve config vars
-fi
-
-apt install -qy ${SYSTEM_DEPS}
+apt install -qy ${SYSTEM_DEPENDENCIES:=}
 install --mode=0555 --owner=grocker /tmp/grocker/compile.py /home/grocker/compile.py
 install --mode=0777 --owner=grocker -d /home/grocker/packages
 
