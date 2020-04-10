d2caabc140d31a51bb0278147b52d015734e9b5c
Fabien Bochu <fabien.bochu@polyconseil.fr>
2020-04-10 11:46:57 +0200

    Simplify setuptools config

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 #! /usr/bin/env python
 from setuptools import setup
 
-setup(
-    setup_requires=['setuptools>=30.3'],
-    package_dir={  # FIXME: wait for https://github.com/pypa/setuptools/issues/1136
-        '': 'src',
-    },
-)
+setup()
