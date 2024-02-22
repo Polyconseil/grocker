10d4c6de85db50ad598183545f76b69ff1be4242
Alexandre Muller <alexandre.muller@polyconseil.fr>
2024-02-22 11:00:08 +0100

    Drop support for Python 3.7
    importlib.resources exists in Python 3.7, thus removing backport dependency.

--- src/grocker/__init__.py
+++ src/grocker/__init__.py
@@ -1,24 +1,6 @@
 # Copyright (c) Polyconseil SAS. All rights reserved.
 
-import sys
-import warnings
-
-try:
-    from importlib import metadata
-except ImportError:
-    # Python<3.8: use importlib-metadata package
-    import importlib_metadata as metadata  # type: ignore
+from importlib import metadata
 
 __version__ = metadata.version('grocker')
 __copyright__ = '2015, Polyconseil'
-
-
-class GrockerDeprecationWarning(Warning):
-    pass
-
-
-if tuple(sys.version_info[:2]) in ((3, 5)):
-    warnings.warn(
-        "Support for 3.5 will be dropped in next major version",
-        category=GrockerDeprecationWarning,
-    )
